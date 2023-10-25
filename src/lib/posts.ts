dc546e881222007b760075ceffb8f138090626b3
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-10-25 21:16:24 +0300

    feat: add mobile view

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -74,33 +74,3 @@ export async function getPostBySlug(slug: string) {
   const html = marked.parse(content);
   return html;
 }
-
-export async function getPostById(id: number): Promise<Post> {
-  const files = await fs.readdir(POSTS_DIR);
-  let idx = id - 1;
-  if (files.length <= idx) throw new Error(`"${id}" is not a valid file id`);
-
-  const file = files[idx];
-
-  const post: Post = await fs
-    .readFile(path.join(POSTS_DIR, file))
-    .then((buf) => buf.toString())
-    .then((content) =>
-      content.replace(/\u200f|\u200B|\u200D|\u200E|\uFEFF/gim, "")
-    )
-    .then((raw) => {
-      const { attributes, body } = fm<Partial<Attributes>>(raw);
-      const html = marked.parse(body);
-      const { title } = attributes;
-
-      if (!title) throw new Error("Missing a blog title");
-
-      return {
-        id,
-        body: html,
-        title,
-      };
-    });
-
-  return post;
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "src/app/components/Layout";
 import { AppProps } from "next/app";
 import "src/app/globals.css";
-import { Post, getPosts } from "src/lib/posts";
+import { Post } from "src/lib/posts";
 
 const App: React.FC<AppProps & { posts: Post[] }> = ({
   Component,
