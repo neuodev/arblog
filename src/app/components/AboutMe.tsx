37ea5ce37a34e7c4bf9b4ba5a5e35448381f0d77
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-10-25 22:59:21 +0300

    fix: use static import paths for images

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -2,35 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import text from "src/data/text.json";
+import pic from "src/images/pic.png";
+import github from "src/images/github.svg";
+import gmail from "src/images/gmail.svg";
+import linkedin from "src/images/linkedin.svg";
+import youtube from "src/images/youtube.svg";
+import portfolio from "src/images/portfolio.svg";
+import medium from "src/images/medium.svg";
 
 const SOCIAL_LINKS = [
   {
-    icon: "/airbrakedotio.svg",
+    icon: portfolio,
     text: "Portfolio",
     link: "https://me.ahmedibrahim.dev/",
   },
   {
-    icon: "/medium.svg",
+    icon: medium,
     text: "Medium",
     link: "https://blog.ahmedibrahim.dev/",
   },
   {
-    icon: "/github.svg",
+    icon: github,
     text: "GitHub",
     link: "https://github.com/neuodev",
   },
   {
-    icon: "/gmail.svg",
+    icon: gmail,
     text: "Email",
     link: "mailto:me@ahmedibrahim.dev",
   },
   {
-    icon: "/linkedin.svg",
+    icon: linkedin,
     text: "LinkedIn",
     link: "https://www.linkedin.com/in/neuodev/",
   },
   {
-    icon: "/youtube.svg",
+    icon: youtube,
     text: "YouTube",
     link: "https://www.youtube.com/@neuodev",
   },
@@ -39,12 +46,7 @@ const SOCIAL_LINKS = [
 const AboutMe: React.FC<{}> = () => {
   return (
     <div className="flex flex-col justify-center items-center sm:flex-row gap-4">
-      <Image
-        width={200}
-        height={200}
-        src="/ahmed-ibrahim.png"
-        alt={text.ahmedIbrahim}
-      />
+      <Image width={200} height={200} src={pic} alt={text.ahmedIbrahim} />
 
       <div className="sm:pt-10">
         <h1 className="mb-1 text-sm">{text.aboutme.header}</h1>
