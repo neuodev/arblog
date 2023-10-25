2642ed33b9e60362f0b1504b605e68482ccb063f
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-10-25 20:47:56 +0300

    feat: render posts list

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,11 +1,7 @@
 import React, { ReactNode } from "react";
 
 const Layout: React.FC<{ children?: ReactNode }> = ({ children }) => {
-  return (
-    <div className="font-kufi bg-dark text-gray-100 min-h-screen">
-      {children}
-    </div>
-  );
+  return <div className="font-kufi text-gray-800 min-h-screen">{children}</div>;
 };
 
 export default Layout;
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,35 @@
+import { GetStaticPropsContext } from "next";
+import React from "react";
+import { getPostBySlug } from "src/lib/posts";
+import blog from "src/data/blog.json";
+
+const Post: React.FC<{ content: string }> = ({ content }) => {
+  return (
+    <div>
+      <div id="post" className="max-w-screen-md mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ slug: string }>
+) {
+  const slug = context.params?.slug;
+  if (!slug) throw new Error("Invalid or missing post slug");
+  const content = await getPostBySlug(slug);
+
+  return {
+    props: { content },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: blog.posts.map((post) => `/${post.slug}`),
+    fallback: "blocking",
+  };
+}
