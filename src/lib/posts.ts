0710c5701f9692dd1c538143024453f4641f3e61
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-10-25 20:18:55 +0300

    feat: parse post title & preview

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -2,6 +2,8 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import marked from "marked";
 import fm from "front-matter";
+import blog from "src/data/blog.json";
+import assert from "node:assert";
 
 // note: this fille will be used from '.next/server'
 const POSTS_DIR = path.join(__dirname, "../../../src/posts");
@@ -16,32 +18,49 @@ export type Post = {
   title: string;
 };
 
-export async function getPosts(): Promise<Array<Post>> {
-  const files = await fs.readdir(POSTS_DIR);
+export type IPost = {
+  slug: string;
+  filename: string;
+  header: string;
+  preview: string;
+};
 
-  const posts: Array<Post> = await Promise.all(
-    files.map(
-      async (file, idx) =>
-        await fs
-          .readFile(path.join(POSTS_DIR, file))
-          .then((buf) => buf.toString())
-          .then((content) =>
-            content.replace(/\u200f|\u200B|\u200D|\u200E|\uFEFF/gim, "")
-          )
-          .then((raw) => {
-            const { attributes, body } = fm<Partial<Attributes>>(raw);
-            const html = marked.parse(body);
-            const title = attributes.title;
+async function readPostFile(filename: string): Promise<string> {
+  const buffer = await fs.readFile(path.join(POSTS_DIR, filename));
+  const content = buffer.toString();
+  return content.replace(/\u200f|\u200B|\u200D|\u200E|\uFEFF/gim, "");
+}
 
-            if (!title) throw new Error("Missing a blog title");
+// 1. use slug
+// 2. render posts
+// 3. render post
 
-            return {
-              id: idx + 1,
-              body: html,
-              title,
-            };
-          })
-    )
+export async function getPosts(): Promise<Array<IPost>> {
+  const posts = await Promise.all(
+    blog.posts.map(async (post) => {
+      const content = await readPostFile(post.filename);
+      const ast = marked.lexer(content);
+      // 1st token MUST be "heading"
+      // 2nd token MUST be "paragraph"
+      const [header, preview] = ast;
+
+      assert(
+        header.type === "heading",
+        `Unexpecting token: ${header.type}, expecting "heading"`
+      );
+
+      assert(
+        preview.type === "paragraph",
+        `Unexpecting token: ${preview.type}, expecting "heading"`
+      );
+
+      return {
+        slug: post.slug,
+        filename: post.filename,
+        header: header.text,
+        preview: preview.text,
+      };
+    })
   );
 
   return posts;
