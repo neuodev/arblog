6f07efe73f4907745c9de887be9d621fd16b00e9
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-11-06 10:53:15 +0200

    feat: style code html inline element

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -3,7 +3,10 @@ import ThemeSwitcher from "./ThemeSwitcher";
 
 const Layout: React.FC<{ children?: ReactNode }> = ({ children }) => {
   return (
-    <div className="font-kufi text-gray-800 min-h-screen dark:bg-slate-800 dark:text-slate-500">
+    <div
+      dir="rtl"
+      className="font-kufi text-gray-800 min-h-screen dark:bg-slate-800 dark:text-slate-500"
+    >
       <ThemeSwitcher />
       {children}
     </div>
