fd6a85b142f9c8d5abb96470d9aa035ddbabd20f
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-11-13 22:37:26 +0200

    fix: update nextjs config to avoid optimizing images as build time

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.CI ? "export" : undefined,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
