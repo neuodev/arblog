b688fe0540dfd7b25f6608d6e686c0a433b77cb0
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-11-14 08:46:50 +0200

    feat: add https server support

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import connectDB from "./config/db";
 import emailRouter from "./routes/email";
 import errorHandler from "./middleware/error";
+import https from "node:https";
+import fs from "node:fs";
 
 connectDB();
 const app = express();
@@ -19,6 +21,23 @@ app.get("/", (_req, res) => {
 // api error handler (should be at the very end)
 app.use(errorHandler);
 
-// Run the server
+const enableHttps: boolean = !!process.env.ENABLE_HTTPS;
+const keyFilePath = process.env.KEY_FILE_PATH;
+const certFilePath = process.env.CERT_FILE_PATH;
+const chainFilePath = process.env.CHAIN_FILE_PATH;
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server running on port ${port}`.bgCyan));
+
+if (enableHttps && keyFilePath && certFilePath && chainFilePath) {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync(keyFilePath),
+        cert: fs.readFileSync(certFilePath),
+        ca: fs.readFileSync(chainFilePath),
+      },
+      app
+    )
+    .listen(443, () => console.log("Listening on 443 (https)".bgCyan));
+} else {
+  app.listen(port, () => console.log(`Server running on port ${port}`.bgCyan));
+}
