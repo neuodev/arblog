9cf155bf7d3bbc33c081fdfde04a70fee376dc58
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-11-17 22:13:28 +0200

    fix(client): format the date correctly

--- client/src/lib/date.ts
+++ client/src/lib/date.ts
@@ -2,5 +2,5 @@ import dayjs from "dayjs";
 import "dayjs/locale/ar";
 
 export function formatDate(date: string) {
-  return dayjs(date).locale("ar").format("M MMM YYYY");
+  return dayjs(date).locale("ar").format("D MMM YYYY");
 }
