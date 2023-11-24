d5c2598c560f4d944afe00319b9139f1a66fc251
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-11-24 08:10:05 +0200

    feat(client): add private posts support

--- client/scripts/newPost.ts
+++ client/scripts/newPost.ts
@@ -1,17 +1,39 @@
-import blog from "../src/data/blog.json";
+import publicPosts from "../src/data/blog.json";
+import privatePosts from "../src/data/blog.private.json";
 import fs from "node:fs/promises";
 import path from "node:path";
 import dayjs from "dayjs";
 import { exec } from "node:child_process";
 
 const POSTS = "../src/posts";
+const PRIVATE_POSTS_LIST = "../src/data/blog.private.json";
+const PUBLIC_POSTS_LIST = "../src/data/blog.json";
 
-async function main() {
-  const slug = process.argv[2];
-  const open = !!process.argv[3];
+function getOps(): {
+  slug: string;
+  open: boolean;
+  isPrivate: boolean;
+} {
+  const args = process.argv;
+  const slug = args[2];
   if (!slug) throw new Error("Missing post slug");
 
-  const filename = slug.concat(".md");
+  const open = !!args.find((arg) => arg === "open" || "-o" || "--open");
+  const isPrivate = !!args.find(
+    (arg) => arg === "private" || "--private" || "-p" || "--prv"
+  );
+
+  return {
+    slug,
+    open,
+    isPrivate,
+  };
+}
+
+async function main() {
+  const { slug, open, isPrivate } = getOps();
+
+  const filename = isPrivate ? slug.concat(".private.md") : slug.concat(".md");
   const filePath = path.join(__dirname, POSTS, filename);
 
   await fs.writeFile(
@@ -19,16 +41,20 @@ async function main() {
     [`# new::post::${slug}`, "happy writing!!"].join("\n\n")
   );
 
+  const posts = isPrivate ? privatePosts : publicPosts;
+
   const post = {
     slug,
     filename,
     published: false,
     createdAt: dayjs().format("MMM D, YYYY"),
   };
-  blog.posts.unshift(post);
+
+  posts.posts.unshift(post);
+
   await fs.writeFile(
-    path.join(__dirname, "../src/data/blog.json"),
-    JSON.stringify(blog, null, 2)
+    path.join(__dirname, isPrivate ? PRIVATE_POSTS_LIST : PUBLIC_POSTS_LIST),
+    JSON.stringify(posts, null, 2)
   );
 
   if (open) exec(`open -a TextEdit ${filePath}`);
