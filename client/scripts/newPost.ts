2609f338fc777ec0dde483086a47954cfad3a660
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-01-15 20:28:33 +0200

    chore: remove private posts feature

--- client/scripts/newPost.ts
+++ client/scripts/newPost.ts
@@ -1,47 +1,38 @@
-import publicPosts from "../src/data/blog.json";
-import privatePosts from "../src/data/blog.private.json";
+import posts from "../src/data/blog.json";
 import fs from "node:fs/promises";
 import path from "node:path";
 import dayjs from "dayjs";
 import { exec } from "node:child_process";
 
 const POSTS = "../src/posts";
-const PRIVATE_POSTS_LIST = "../src/data/blog.private.json";
 const PUBLIC_POSTS_LIST = "../src/data/blog.json";
 
 function getOps(): {
   slug: string;
   open: boolean;
-  isPrivate: boolean;
 } {
   const args = process.argv;
   const slug = args[2];
   if (!slug) throw new Error("Missing post slug");
 
   const open = args.some((arg) => ["open", "-o", "--open"].includes(arg));
-  const isPrivate = args.some((arg) =>
-    ["private", "--private", "-p", "--prv"].includes(arg)
-  );
 
   return {
     slug,
     open,
-    isPrivate,
   };
 }
 
 async function main() {
-  const { slug, open, isPrivate } = getOps();
+  const { slug, open } = getOps();
 
-  const filename = isPrivate ? slug.concat(".private.md") : slug.concat(".md");
+  const filename = slug.concat(".md");
   const filePath = path.join(__dirname, POSTS, filename);
 
   await fs.writeFile(
     filePath,
     [`# new::post::${slug}`, "happy writing!!"].join("\n\n")
   );
-
-  const posts = isPrivate ? privatePosts : publicPosts;
 
   const post = {
     slug,
@@ -53,7 +44,7 @@ async function main() {
   posts.posts.unshift(post);
 
   await fs.writeFile(
-    path.join(__dirname, isPrivate ? PRIVATE_POSTS_LIST : PUBLIC_POSTS_LIST),
+    path.join(__dirname, PUBLIC_POSTS_LIST),
     JSON.stringify(posts, null, 2)
   );
 
